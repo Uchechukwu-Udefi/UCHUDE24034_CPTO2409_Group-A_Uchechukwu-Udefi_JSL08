70a6c870c0074146ec65ea6d3e6fdc82d1712419
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-03-05 10:38:34 +0200

    Update bank branch class

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
+const bankBranchInstance = null;
 
 // 2. Define a class called `BankBranch` for managing branch information.
+    class BankBranch {
 
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
+        constructor(branchInfo) {
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
+            if (bankBranchInstance === null) {
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
+                bankBranchInstance = branchInfo;
+            }
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
+            return bankBranchInstance;
+        }
+    
 
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
